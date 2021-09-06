696dea2c6174bf3b42b519d27a0c389aa02744c2
Brabitt <ronnyanchaluisa@gmail.com>
2021-09-06 16:37:14 +0200

    Preparing the training data

--- chatbot.py
+++ chatbot.py
@@ -1,7 +1,9 @@
 import json
 import pickle
+import random
 
 import nltk
+import numpy as np
 from nltk.stem import WordNetLemmatizer
 
 lemmatizer = WordNetLemmatizer()
@@ -24,15 +26,37 @@ def tokenize_text():
                 classes.append(intent['tag'])
     print(documents)
     # lemmatize the words and classes
-    words = [lemmatizer.lemmatize(word) for word in words if word not in ignore_letters]
+    words = [lemmatizer.lemmatize(word.lower()) for word in words if word not in ignore_letters]
     words = sorted(set(words))
     classes = sorted(set(classes))
     print(words)
     print(classes)
 
-    # saving the lists words and classes in files, that will be using in the ML
+    # saving the lists words and classes in files, that will be using in the ML training
     pickle.dump(words, open('words.pkl', 'wb'))
     pickle.dump(classes, open('classes.pkl', 'wb'))
 
+    # Preparing the training data for the chatbot
+    # here we replace the letters by number and store them in an array
+    training = []
+    output_empty = [0] * len(classes)
 
-tokenize_text()
+    for document in documents:
+        bag = []
+        words_patterns = document[0]
+        words_patterns = [lemmatizer.lemmatize(word.lower()) for word in words_patterns]
+        for word in words:
+            if word in words_patterns:
+                bag.append(1)
+            else:
+                bag.append(0)
+
+        output_row = list(output_empty)
+        output_row[classes.index(document[1])] = 1
+        training.append([bag, output_row])
+
+    random.shuffle(training)
+    training = np.array(training)  # create train and test lists. X - patterns, Y - intents
+    train_x = list(training[:, 0])
+    train_y = list(training[:, 1])
+    print("Training data created")
